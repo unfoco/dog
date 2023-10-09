c0f7c3b623609614442f84260c80a7360e7b539b
schphe <git@emin.sh>
2023-10-09 22:13:03 +0300

    update traits

--- src/util/traits/channel.rs
+++ src/util/traits/channel.rs
@@ -4,12 +4,19 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait ExtendChannelId {
-    async fn send_message_content(&self, http: impl AsRef<serenity::Http> + Send + Sync, content: impl ToString + Send) -> serenity::Result<Message>;
+    async fn send_message_content<H, C>(&self, http: H, content: C) -> serenity::Result<Message>
+        where
+            H: AsRef<serenity::Http> + Send + Sync,
+            C: ToString + Send;
 }
 
 #[async_trait]
 impl ExtendChannelId for serenity::ChannelId {
-    async fn send_message_content(&self, http: impl AsRef<serenity::Http> + Send + Sync, content: impl ToString + Send) -> serenity::Result<Message> {
+    async fn send_message_content<H, C>(&self, http: H, content: C) -> serenity::Result<Message>
+        where
+            H: AsRef<serenity::Http> + Send + Sync,
+            C: ToString + Send
+    {
         self.send_message(http, |c| {
             c.content(content)
         }).await
--- src/util/traits/context.rs
+++ src/util/traits/context.rs
@@ -8,39 +8,56 @@ use super::ExtendChannelId;
 
 #[async_trait]
 pub trait ExtendContext {
-    async fn log_mem(&self, content: impl ToString + Send) -> serenity::Result<Message>;
-    async fn log_mem_with_embed<F>(&self, content: impl ToString + Send, f: F) -> serenity::Result<Message>
-        where F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync;
-    async fn log_sys(&self, content: impl ToString + Send) -> serenity::Result<Message>;
-    async fn log_sys_with_embed<F>(&self, content: impl ToString + Send, f: F) -> serenity::Result<Message>
-        where F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync;
+    async fn log_mem<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send;
+    async fn log_mem_with_embed<C, F>(&self, content: C, f: F) -> serenity::Result<Message>
+        where
+            C: ToString + Send,
+            F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync;
+    async fn log_sys<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send;
+    async fn log_sys_with_embed<C, F>(&self, content: C, f: F) -> serenity::Result<Message>
+        where
+            C: ToString + Send,
+            F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync;
 
-    async fn send_message(&self, content: impl ToString + Send) -> serenity::Result<Message>;
+    async fn send_message<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send;
 }
 
 #[async_trait]
 impl ExtendContext for types::AppContext<'_> {
-    async fn log_mem(&self, content: impl ToString + Send) -> serenity::Result<Message> {
+    async fn log_mem<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send
+    {
         self.data.log_mem(self.http(), content).await
     }
 
-    async fn log_mem_with_embed<F>(&self, content: impl ToString + Send, f: F) -> serenity::Result<Message>
-        where F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync
+    async fn log_mem_with_embed<C, F>(&self, content: C, f: F) -> serenity::Result<Message>
+        where
+            C: ToString + Send,
+            F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync
     {
         self.data.log_mem_with_embed(self.http(), content, f).await
     }
 
-    async fn log_sys(&self, content: impl ToString + Send) -> serenity::Result<Message> {
+    async fn log_sys<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send
+    {
         self.data.log_sys(self.http(), content).await
     }
 
-    async fn log_sys_with_embed<F>(&self, content: impl ToString + Send, f: F) -> serenity::Result<Message>
-        where F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync
+    async fn log_sys_with_embed<C, F>(&self, content: C, f: F) -> serenity::Result<Message>
+        where
+            C: ToString + Send,
+            F: FnOnce(&mut CreateEmbed) -> &mut CreateEmbed + Send + Sync
     {
         self.data.log_sys_with_embed(self.http(), content, f).await
     }
 
-    async fn send_message(&self, content: impl ToString + Send) -> serenity::Result<Message> {
+    async fn send_message<C>(&self, content: C) -> serenity::Result<Message>
+        where C: ToString + Send
+    {
         self.channel_id().send_message_content(self.http(), content).await
     }
 }
