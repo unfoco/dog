6d409f9b0f20ab1d204e648964b2ffa1459c2fe7
umitc18 <umitc18@hotmail.com>
2024-10-01 15:23:36 +0300

    - Added message.rs for sending specific lines as messages from the provided GitHub link. - Included reqwest and regex crates in the project.

---
+++ src/handler/event/message.rs
@@ -0,0 +1,68 @@
+use poise::serenity_prelude::{Message, Context};
+use reqwest::get;
+use regex::Regex;
+
+use crate::types;
+
+pub async fn handle(
+    ctx: &Context,
+    _framework: types::FrameworkContext<'_>,
+    data: &types::Data,
+    message: &Message,
+) -> Result<(), types::Error> {
+    if message.author.bot {
+        return Ok(());
+    }
+
+    let msg = message.content.clone();
+
+    let re = Regex::new(r"https://github\.com/(?P<user>[^/]+)/(?P<repo>[^/]+)/blob/(?P<path>.+?)/(?P<file>[^/]+)\.(?P<extension>[^#]+)#L(?P<start_line>\d+)(?:-L(?P<end_line>\d+))?").unwrap();
+
+    if let Some(captures) = re.captures(&msg) {
+        let user = &captures["user"];
+        let repo = &captures["repo"];
+        let path = &captures["path"];
+        let file = &captures["file"];
+        let extension = &captures["extension"];
+        let start_line = &captures["start_line"];
+        let end_line = captures.name("end_line").map(|m| m.as_str()).unwrap_or(start_line);
+
+        let url = format!(
+            "https://raw.githubusercontent.com/{}/{}/{}/{}.{}",
+            user, repo, path, file, extension
+        );
+
+        let res = get(&url).await?;
+        let res_text = res.text().await?;
+
+        let base = start_line.parse::<i32>().unwrap();
+        let end = end_line.parse::<i32>().unwrap();
+        if end == base {
+            let start = base - 5;
+            let end = base + 5;
+            let mut lines = res_text.lines().enumerate().filter(|(i, _)| {
+                *i >= start as usize && *i <= end as usize
+            });
+
+            let mut result = String::new();
+            while let Some((i, line)) = lines.next() {
+                result.push_str(&format!("{}: {}\n", i, line));
+            }
+
+            message.reply(&ctx.http, format!("```{}\n{}\n```", extension, result)).await?;
+        } else {
+            let mut lines = res_text.lines().enumerate().filter(|(i, _)| {
+                *i >= base as usize && *i <= end as usize
+            });
+
+            let mut result = String::new();
+            while let Some((i, line)) = lines.next() {
+                result.push_str(&format!("{}: {}\n", i, line));
+            }
+
+            message.reply(&ctx.http, format!("```{}\n{}\n```", extension, result)).await?;
+        }
+    }
+
+    Ok(())
+}
